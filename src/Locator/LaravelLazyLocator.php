a19de63dfa67560626e4b19961d4013b59bab54c
Koldo Picaza <Koldo@Mac-mini-de-Koldo.local>
2017-08-09 16:44:11 +0200

    add basic tests to lazy locator

--- src/Locator/LaravelLazyLocator.php
+++ src/Locator/LaravelLazyLocator.php
@@ -4,8 +4,21 @@ namespace Joselfonseca\LaravelTactician\Locator;
 
 use League\Tactician\Exception\MissingHandlerException;
 
-class LaravelLazyLocator extends LaravelLocator
+class LaravelLazyLocator implements LocatorInterface
 {
+
+    /**
+     * The handlers
+     * @var
+     */
+    protected $handlers;
+
+    /**
+     * Bind a handler instance to receive all commands with a certain class
+     *
+     * @param string $handler          Handler to receive class name
+     * @param string $commandClassName Command class e.g. "My\TaskAddedCommand"
+     */
     public function addHandler($handler, $commandClassName)
     {
         $this->handlers[$commandClassName] = function () use ($handler) {
@@ -13,6 +26,33 @@ class LaravelLazyLocator extends LaravelLocator
         };
     }
 
+    /**
+     * Allows you to add multiple handlers at once.
+     *
+     * The map should be an array in the format of:
+     *  [
+     *      AddTaskCommand::class      => $someHandlerClassName,
+     *      CompleteTaskCommand::class => $someHandlerClassName,
+     *  ]
+     *
+     * @param array $commandClassToHandlerMap
+     */
+    public function addHandlers(array $commandClassToHandlerMap)
+    {
+        foreach ($commandClassToHandlerMap as $commandClass => $handler) {
+            $this->addHandler($handler, $commandClass);
+        }
+    }
+
+    /**
+     * Retrieves the handler for a specified command
+     *
+     * @param string $commandName
+     *
+     * @return object
+     *
+     * @throws MissingHandlerException
+     */
     public function getHandlerForCommand($commandName)
     {
         if (!isset($this->handlers[$commandName])) {
--- tests/Locator/TestLaravelLazyLocator.php
+++ tests/Locator/TestLaravelLazyLocator.php
@@ -2,20 +2,28 @@
 
 namespace Joselfonseca\LaravelTactician\Tests\Locator;
 
+use Illuminate\Support\Facades\Config;
+use Joselfonseca\LaravelTactician\Locator\LaravelLazyLocator;
 use Joselfonseca\LaravelTactician\Tests\TestCase;
 
 /**
  * Class TestLaravelLocator
  * @package Joselfonseca\LaravelTactician\Tests\Locator
  */
-class TestLaravelLazyLocator extends TestCase{
+class TestLaravelLazyLocator extends TestCase
+{
+
+    public function setUp() {
+        parent::setUp();
+        Config::set('locator', LaravelLazyLocator::class);
+    }
 
     /**
      * It resolves the locator
      */
     public function test_it_resolves_the_laravel_locator()
     {
-        $this->assertInstanceOf('League\Tactician\Handler\Locator\HandlerLocator',
+        $this->assertInstanceOf('Joselfonseca\LaravelTactician\Locator\LocatorInterface',
             app('Joselfonseca\LaravelTactician\Locator\LaravelLazyLocator'));
     }
 
@@ -25,7 +33,7 @@ class TestLaravelLazyLocator extends TestCase{
      */
     public function test_it_throws_exception_when_locator_from_laravel_container_is_not_found()
     {
-        $locator = app('League\Tactician\Handler\Locator\HandlerLocator');
+        $locator = app('Joselfonseca\LaravelTactician\Locator\LocatorInterface');
         $handler = $locator->getHandlerForCommand('TestCommand');
     }
 
@@ -35,7 +43,7 @@ class TestLaravelLazyLocator extends TestCase{
      */
     public function test_it_throws_exception_when_locator_is_not_resolve_from_laravel_container()
     {
-        $locator = app('League\Tactician\Handler\Locator\HandlerLocator');
+        $locator = app('Joselfonseca\LaravelTactician\Locator\LocatorInterface');
         $locator->addHandler('SomeCommandHandler',
             'Joselfonseca\LaravelTactician\Tests\Stubs\TestCommand');
     }
@@ -45,11 +53,26 @@ class TestLaravelLazyLocator extends TestCase{
      */
     public function test_it_is_able_to_resolve_handler_from_laravel_container()
     {
-        $locator = app('League\Tactician\Handler\Locator\HandlerLocator');
+        $locator = app('Joselfonseca\LaravelTactician\Locator\LocatorInterface');
         $locator->addHandler('Joselfonseca\LaravelTactician\Tests\Stubs\TestCommandHandler',
             'Joselfonseca\LaravelTactician\Tests\Stubs\TestCommand');
         $handler = $locator->getHandlerForCommand('Joselfonseca\LaravelTactician\Tests\Stubs\TestCommand');
         $this->assertInstanceOf('Joselfonseca\LaravelTactician\Tests\Stubs\TestCommandHandler', $handler);
     }
 
+    /**
+     * Add more than one command => handler to the bus
+     */
+    public function test_it_maps_array_commands()
+    {
+        $locator = app('Joselfonseca\LaravelTactician\Locator\LocatorInterface');
+        $locator->addHandlers([
+            'Joselfonseca\LaravelTactician\Tests\Stubs\TestCommand' => 'Joselfonseca\LaravelTactician\Tests\Stubs\TestCommandHandler',
+            'Joselfonseca\LaravelTactician\Tests\Stubs\TestCommandInput' => 'Joselfonseca\LaravelTactician\Tests\Stubs\TestCommandSeccondHandler'
+        ]);
+        $handler = $locator->getHandlerForCommand('Joselfonseca\LaravelTactician\Tests\Stubs\TestCommand');
+        $handler2 = $locator->getHandlerForCommand('Joselfonseca\LaravelTactician\Tests\Stubs\TestCommandInput');
+        $this->assertInstanceOf('Joselfonseca\LaravelTactician\Tests\Stubs\TestCommandHandler', $handler);
+        $this->assertInstanceOf('Joselfonseca\LaravelTactician\Tests\Stubs\TestCommandSeccondHandler', $handler2);
+    }
 }
